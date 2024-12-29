dcd4c488348b3b96b3381c6321c8c82c4638b8c6
Bakhadirov-Jaloliddin-25 <jboxodirov4@gmail.com>
2024-12-30 02:09:34 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Todo from "./components/todo/TOdo";
+import Users from "./components/todo/users/Users";
+
+const App = () => {
+  return (
+    <div>
+      {/* <h2>App</h2> */}
+      {/* <Todo /> */}
+      <Users />
+    </div>
+  );
+};
+
+export default App;
